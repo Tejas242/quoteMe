03438868d4eb0efad91b4769976a088fe32c8db5
Tejas Mahajan <tmahajan09876@gmail.com>
2023-12-01 18:52:49 +0530

    blank

--- src/components/CodeLink.tsx
+++ src/components/CodeLink.tsx
@@ -1,7 +1,7 @@
 export default function CodeLink() {
   return (
     <div className="absolute bottom-0 text-center mb-4">
-        <p className="text-gray-600">Check<a className="transition-all duration-100 ease-out  hover:scale-10 hover:text-blue-500" href="https://github.com/Tejas242/quoteMe" target="_blank" rel="norefferer noopener">Source Code</a></p>
+        <p className="text-gray-600">Check <a className="transition-all duration-100 ease-out  hover:scale-10 hover:text-blue-500" href="https://github.com/Tejas242/quoteMe" target="_blank" rel="norefferer noopener">Source Code</a></p>
     </div>
   );
 }
